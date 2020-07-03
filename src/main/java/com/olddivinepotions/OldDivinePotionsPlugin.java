a5216a3c0ec3b41d52b0c78997c6d08ca1ae3229
Parasailer <rs.multithreads@gmail.com>
2020-07-03 13:33:28 -0700

    Changed resources path and started ideas for overrides

--- src/main/java/com/olddivinepotions/OldDivinePotionsPlugin.java
+++ src/main/java/com/olddivinepotions/OldDivinePotionsPlugin.java
@@ -1,20 +1,21 @@
 package com.olddivinepotions;
 
-import com.google.inject.Provides;
+import java.awt.image.BufferedImage;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
-import net.runelite.client.config.ConfigManager;
-import net.runelite.client.eventbus.Subscribe;
+import net.runelite.api.SpritePixels;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.util.ImageUtil;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Old Divine Potions",
+	description = "Reverts the appearance of divine potions to their pre-July 2020 look.",
+	tags = {"divine-pots", "egniol", "appearance", "reskin", "traditional", "icons", "inventory", "bank"},
+	enabledByDefault = false
 )
 public class OldDivinePotionsPlugin extends Plugin
 {
@@ -22,32 +23,52 @@ public class OldDivinePotionsPlugin extends Plugin
 	private Client client;
 
 	@Inject
-	private OldDivinePotionsConfig config;
+	private ClientThread clientThread;
 
 	@Override
 	protected void startUp() throws Exception
 	{
-		log.info("Example started!");
+		clientThread.invoke(this::overrideItems);
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.info("Example stopped!");
+		clientThread.invoke(this::restoreItems);
 	}
 
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	private SpritePixels getFileSpritePixels(String file)
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+		try
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+			log.debug("Loading: {}", file);
+			BufferedImage image = ImageUtil.getResourceStreamFromClass(this.getClass(), file);
+			return ImageUtil.getImageSpritePixels(image, client);
+		}
+		catch (RuntimeException ex)
+		{
+			log.debug("Unable to load image: ", ex);
+		}
+
+		return null;
+	}
+
+	private void overrideItems()
+	{
+		for (ItemOverride itemOverride : ItemOverride.values())
+		{
+			String file = itemOverride.getItemID() + ".png";
+			SpritePixels spritePixels = getFileSpritePixels(file);
+			client.getSpriteOverrides().put(itemOverride.getItemID(), spritePixels);
 		}
 	}
 
-	@Provides
-	OldDivinePotionsConfig provideConfig(ConfigManager configManager)
+	private void restoreItems()
 	{
-		return configManager.getConfig(OldDivinePotionsConfig.class);
+		client.getWidgetSpriteCache().reset();
+		for (ItemOverride itemOverride : ItemOverride.values())
+		{
+			client.getSpriteOverrides().remove(itemOverride.getItemID());
+		}
 	}
 }
