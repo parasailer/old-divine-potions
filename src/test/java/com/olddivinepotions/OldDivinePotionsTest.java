8e148f4a2c0c8d4a85e44a07f1343807a9c5983d
Parasailer <rs.multithreads@gmail.com>
2020-07-02 23:21:24 -0700

    Added base art, no code yet

--- src/test/java/com/olddivinepotions/OldDivinePotionsTest.java
+++ src/test/java/com/olddivinepotions/OldDivinePotionsTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.olddivinepotions;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class OldDivinePotionsTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(OldDivinePotionsPlugin.class);
 		RuneLite.main(args);
 	}
 }
